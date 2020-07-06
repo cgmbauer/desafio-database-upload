8e1c9376ecc3c4277e3ec46b1b5e20f481da378d
cgmbauer <cgmbauer@gmail.com>
2020-07-06 19:47:35 -0300

    Import csv and export data to database.

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,69 @@
-import Transaction from '../models/Transaction';
+import csvParse from 'csv-parse';
+import fs from 'fs';
 
+import { getCustomRepository } from 'typeorm';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateCategoryService from './CreateCategoryService';
+
+interface Transactions {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category_id: string;
+}
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(filePath: string): Promise<Transactions[]> {
+    const readCSVStream = fs.createReadStream(filePath);
+
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+
+    const parseCSV = readCSVStream.pipe(parseStream);
+
+    const lines: any[] = [];
+
+    parseCSV.on('data', line => {
+      lines.push(line);
+    });
+
+    await new Promise(resolve => {
+      parseCSV.on('end', resolve);
+    });
+
+    const createCategory = new CreateCategoryService();
+
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transactions = [];
+
+    for (const line of lines) {
+      const [title, type, value, category] = line;
+
+      const newCategory = await createCategory.execute({
+        categoryTitle: category,
+      });
+
+      const arrayToObject = {
+        title,
+        type,
+        value,
+        category_id: newCategory.id,
+      };
+
+      transactions.push(arrayToObject);
+
+      const transaction = transactionRepository.create(arrayToObject);
+
+      await transactionRepository.save(transaction);
+    }
+
+    await fs.promises.unlink(filePath);
+
+    return transactions;
   }
 }
 
