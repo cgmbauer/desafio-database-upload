28be53bd33238a4afc05989129f230c7fdac6876
cgmbauer <cgmbauer@gmail.com>
2020-07-06 19:45:52 -0300

    Can delete transaction from database.

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,25 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface RequestDTO {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: RequestDTO): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transactionToBeDeleted = await transactionRepository.findOne({
+      where: { id },
+    });
+
+    if (!transactionToBeDeleted) {
+      throw new AppError('Repository ID does not exist.');
+    }
+
+    await transactionRepository.delete(transactionToBeDeleted.id);
   }
 }
 
